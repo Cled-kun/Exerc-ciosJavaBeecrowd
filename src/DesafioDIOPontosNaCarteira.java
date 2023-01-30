dc93e0ff3102afecf759b1b92875eec0d9891436
Cled-kun <105759035+Cled-kun@users.noreply.github.com>
2023-01-29 21:59:26 -0300

    Update DesafioDIOPontosNaCarteira.java

--- src/DesafioDIOPontosNaCarteira.java
+++ src/DesafioDIOPontosNaCarteira.java
@@ -2,27 +2,21 @@ import java.util.Scanner;
 
 public class DesafioDIOPontosNaCarteira {
     public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-
-        int velocidadePermitida = 80;
-        int PontosNaCarteira = 3;
-        int count = 0;
-
-        do {
-            System.out.println("");
-            int entradaVelocidade = leitor.nextInt();
-            int quantidadeMultas = leitor.nextInt();
-            
-            quantidadeMultas++;
-
-            if (entradaVelocidade >= velocidadePermitida && quantidadeMultas < PontosNaCarteira) {
-                System.out.print(quantidadeMultas + " multas. Nao levou pontos na carteira");
-                
-            } else {
-                System.out.printf(quantidadeMultas + " multas. Levou pontos na carteira");
+        Scanner scan = new Scanner(System.in);
+            int velocidade = scan.nextInt();
+            int multas = scan.nextInt();
+    
+          
+            if (velocidade > 80) {
+                multas++;
             }
             
-            count++;
-        } while (count < 3);
+            System.out.print(multas + " multas.");
+       
+            if (multas >= 3) {
+                System.out.println(" Levou pontos na carteira");
+            } else {
+                System.out.println(" Nao levou pontos na carteira");
+            }
     }
 }
